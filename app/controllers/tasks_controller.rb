a025e85836c48fad4203b19102c08399528b65e4
fahad alkubaisi <alkubaisi9@gmail.com>
2017-02-14 13:35:41 +0000

    done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,22 +1,41 @@
 class TasksController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
   def index
+    @tasks = Task.all
   end
 
   def create
+    task = Task.create(task_params)
+    redirect_to tasks_path
   end
 
   def new
+   @task = Task.new
+ end
+
+ def edit
+
+ end
+
+ def show
+ end
+
+ def update
+  @task.update(task_params)
+  redirect_to tasks_path
+
+end
+
+def destroy
+      @task.destroy
+    redirect_to tasks_path
+end
+def task_params
+    # params[:restaurant] == params.require(:restaurant)
+    params.require(:task).permit(:name, :status)
   end
 
-  def edit
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def destroy
+  def set_restaurant
+    @task = Task.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Task.create(name: "clean house", status: "Not done")
+Task.create(name: "Read Books", status: "Not done")
+Task.create(name: "Play football", status: "Not done")
